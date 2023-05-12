5b070dd480799397aa953bea8c6210c7de2b4af4
wocwin <wocwin@163.com>
2023-05-12 18:16:24 +0800

    fix:清除之前vue2及vue3项目改用wocwin-vue2及wocwin-admin

--- vue3Vite/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-declare module 'jsencrypt/bin/jsencrypt';
-declare module '@wocwin/t-ui-plus';
