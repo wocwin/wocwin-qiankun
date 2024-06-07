29c8f0b3c66e09b642d739b67c051c6b499ca8b5
wocwin <wocwin@163.com>
2024-06-07 18:36:55 +0800

    fix：Vue2静态路由注释

--- wocwin-vue2/src/router/index.js
+++ wocwin-vue2/src/router/index.js
@@ -4,15 +4,6 @@ import Layout from '@/layout'
 import login from '@/views/login.vue'
 import redirect from '@/views/redirect.vue'
 
-/**
- * 静态模块路由
- */
-// import commonRouter from './modules/common' // 常用组件
-// import TTableRouter from './modules/TTable' // TTable组件
-// import TFormRouter from './modules/TForm' // TForm组件
-// import TQueryConditionRouter from './modules/TQuery' // 条件查询组件
-import mapRouter from './modules/map' // 地图路由
-// import complexRouter from './modules/complex' // 组合组件路由
 
 Vue.use(Router)
 
@@ -44,13 +35,7 @@ export const constantRoutes = window.__POWERED_BY_QIANKUN__
 					}
 				}
 			]
-		},
-		// mapRouter,
-		// complexRouter,
-		// commonRouter,
-		// TTableRouter,
-		// TFormRouter,
-		// TQueryConditionRouter
+		}
 	]
 	: [
 		{
@@ -78,13 +63,7 @@ export const constantRoutes = window.__POWERED_BY_QIANKUN__
 			path: '/401',
 			component: () => import('@/views/error/401'),
 			hidden: true
-		},
-		// mapRouter,
-		// complexRouter,
-		// commonRouter,
-		// TTableRouter,
-		// TFormRouter,
-		// TQueryConditionRouter
+		}
 	]
 
 const router = new Router({
